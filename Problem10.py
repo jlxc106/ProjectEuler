7643f67c46d3110b04991591d1d5319a88067d07
jlxc106 <jaylim106@gmail.com>
2016-11-04 01:16:47 -0700

    Problem 10

--- Problem10.py
+++ Problem10.py
@@ -1,17 +1,20 @@
-#! /usr/bin/python
-#Problem statement: https://projecteuler.net/problem=10
-
 import math
+import timeit
 
-def primes(iterable):
 i = 5
-s=5
-LIST = [2,3]
-while i<iterable:
-	if all(i%j != 0 for j in LIST):
+s = 5
+#LIST = [2,3]
+start = timeit.default_timer()
+while i<2000000:
+	t = int(math.sqrt(i))
+	if all(i%j != 0 for j in range(2,t+1)):		#~20 seconds
+	#if all(i%j != 0 for j in LIST if j<t+1):	#~400 seconds
 		s+=i
-		LIST.append(i)
+		#LIST.append(i)
 	i+=2
+stop = timeit.default_timer()
+
 print s
 
-primes(2000000)
+print "time: " + str(stop - start)
+
